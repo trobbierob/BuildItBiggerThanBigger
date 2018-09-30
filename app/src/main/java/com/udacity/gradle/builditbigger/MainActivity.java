0e0eb09ec3431b59423668b6b60c65562a709bc5
trobbierob <troberts@my.hpu.edu>
2018-09-30 15:46:23 -0400

    Add java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.android.javajoker.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "No!", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javajoker/src/main/java/com/example/android/javajoker/Joker.java
@@ -0,0 +1,7 @@
+package com.example.android.javajoker;
+
+public class Joker {
+    public String getJoke(){
+        return "What did the watermelon say to the egg?";
+    }
+}
