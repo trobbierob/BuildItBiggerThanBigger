93f4850e615e7bf1936e62626e497d2d3bf9bf22
trobbierob <troberts@my.hpu.edu>
2018-10-02 19:43:49 -0400

    Connect to gcloud

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,18 +1,17 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
+import com.example.android.jokeactivity.JokeDisplayActivity;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import java.io.IOException;
@@ -65,20 +64,27 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Context... params) {
             if(myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new
+                /*MyApi.Builder builder = new
                         MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                         new AndroidJsonFactory(), null)
                         // options for running against local devappserver
                         // ­ 10.0.2.2 is localhost's IP address in Android emulator
                         // ­ turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setRootUrl("http://192.168.1.10:8080/_ah/api/")
+                        //.setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        //.setRoolUrl("http://<my-computer-address>:8080/_ah/api/")
                         .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                             @Override
                             public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
                                 abstractGoogleClientRequest.setDisableGZipContent(true);
                             }
-                        });
+                        });*/
                 // end options for devappserver
+
+
+                // App Engine
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://finaljokeproject.appspot.com/_ah/api/");
 
                 myApiService = builder.build();
             }
@@ -94,13 +100,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-        /*// Create Intent to launch JokeFactory Activity
-        Intent intent = new Intent(context, DisplayJokeActivity.class);
-        // Put the string in the envelope
-        intent.putExtra(DisplayJokeActivity.JOKE_KEY,result);
-        context.startActivity(intent);
-*/
-            Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+            Intent jokeIntent = new Intent(context, JokeDisplayActivity.class);
+            jokeIntent.putExtra("joke", result);
+            startActivity(jokeIntent);
         }
     }
 }
