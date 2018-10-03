c018ca2616187a344f2e6eb908385d39374eda87
trobbierob <troberts@my.hpu.edu>
2018-10-02 20:56:42 -0400

    Remove some commented code

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -49,11 +48,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        /*Joker joker = new Joker();
-        Intent jokeIntent = new Intent(this, JokeDisplayActivity.class);
-        jokeIntent.putExtra("joke", joker.getJoke());
-        startActivity(jokeIntent);*/
-
         new EndpointAsyncTask().execute(this);
     }
 
@@ -64,28 +58,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Context... params) {
             if(myApiService == null) {  // Only do this once
-                /*MyApi.Builder builder = new
-                        MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // ­ 10.0.2.2 is localhost's IP address in Android emulator
-                        // ­ turn off compression when running against local devappserver
-                        .setRootUrl("http://192.168.1.10:8080/_ah/api/")
-                        //.setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        //.setRoolUrl("http://<my-computer-address>:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });*/
-                // end options for devappserver
-
-
                 // App Engine
                 MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
                         .setRootUrl("https://finaljokeproject.appspot.com/_ah/api/");
-
                 myApiService = builder.build();
             }
 
